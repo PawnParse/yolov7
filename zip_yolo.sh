5092d47cd2dc4f00a4ee681e3400f036b8968cfb
janemrich <jan.emrich@stud.tu-darmstadt.de>
2024-04-11 21:16:05 +0200

    improve zip script

--- zip_yolo.sh
+++ zip_yolo.sh
@@ -1,3 +1,28 @@
-#!sh
+#!/bin/bash
 
-zip -r yolov7.zip ./*
+# Create a temporary directory
+mkdir temp
+
+# Copy all files and directories from 'fine-tuning/' into 'temp/'
+cp -rv ./* temp
+
+# Change directory to 'temp'
+cd temp
+
+# Remove specific directories or files within 'temp'
+rm -r data/datasets/
+# Uncomment the following lines if you want to remove additional directories or files
+# yes | rm -r .git
+# rm -r .ipynb_checkpoints
+rm -r models/*.pt
+rm -r runs/
+rm -r paper
+
+# Zip the contents of 'temp' (but not the 'temp' directory itself) into 'fine-tune-code.zip'
+zip -r ../yolov7.zip ./*
+
+# Change back to the parent directory
+cd ..
+
+# Remove the 'temp' directory
+rm -r temp
