287d3779c399d2bb4cfe9877ee5b06a1fb889628
janemrich <jan.emrich@stud.tu-darmstadt.de>
2024-07-30 21:53:34 +0200

    refactor splitter

--- chess_tools/split_train_val_test.py
+++ chess_tools/split_train_val_test.py
@@ -1,24 +1,26 @@
-import os
+from pathlib import Path
 import random
 import shutil
 
 def create_directory(directory):
     # Create directory if it doesn't exist
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    directory.mkdir(parents=True, exist_ok=True)
 
-def split_dataset(dataset_dir, train_ratio=0.8, val_ratio=0.1, test_ratio=0.1):
-    # Define paths to images and labels directories
-    images_dir = os.path.join(dataset_dir, 'images')
-    labels_dir = os.path.join(dataset_dir, 'labels')
+def split_dataset(dataset_dir, train_ratio=0.8, val_ratio=0.1, test_ratio=0.1, write=False):
+    dataset_path = Path(dataset_dir)
+    images_dir = dataset_path / 'images'
+    labels_dir = dataset_path / 'labels'
 
-    # Get list of image and label files
-    image_files = [filename for filename in os.listdir(images_dir)]
+    # Get list of image files
+    image_files = []
+    for suffix in ['*.jpg', '*.JPG', '*.jpeg', '*.JPEG', '*.png']:
+        image_files.extend(images_dir.glob(suffix))
 
     # Create train, val, and test directories under images and labels folders
-    for folder in ['train', 'val', 'test']:
-        create_directory(os.path.join(images_dir, folder))
-        create_directory(os.path.join(labels_dir, folder))
+    if write:
+        for folder in ['train', 'val', 'test']:
+            create_directory(images_dir / folder)
+            create_directory(labels_dir / folder)
 
     # Shuffle files randomly
     random.shuffle(image_files)
@@ -33,33 +35,35 @@ def split_dataset(dataset_dir, train_ratio=0.8, val_ratio=0.1, test_ratio=0.1):
     val_images = image_files[num_train:num_train + num_val]
     test_images = image_files[num_train + num_val:]
 
-    # Move images and labels to corresponding directories
-    def move_image(file_list, src_dir, dest_dir):
-        for filename in file_list:
-            # Move image file
-            src_image = os.path.join(src_dir, filename)
-            dest_image = os.path.join(dest_dir, filename)
-            shutil.move(src_image, dest_image)
-    
-    def move_label(file_list, src_dir, dest_dir):
-        for filename in file_list:
-            # Move label file
-            base_filename = os.path.splitext(filename)[0]
-            label_filename = f"{base_filename}.txt"
-            src_label = os.path.join(src_dir, label_filename)
-            dest_label = os.path.join(dest_dir, label_filename)
-            shutil.move(src_label, dest_label)
+    def move_files(file_list, src_dir, dest_dir, extension=''):
+        for file_path in file_list:
+            if extension != '':
+                file_path = file_path.with_suffix(extension)
+            src_path = src_dir / file_path.name
+            dest_path = dest_dir / file_path.name
+            if write:
+                shutil.move(src_path, dest_path)
+            else:
+                print(f'mock: mv {src_path}, {dest_path})')
 
     # Move files to train, val, and test directories under images and labels folders
-    move_image(train_images, images_dir, os.path.join(images_dir, 'train'))
-    move_label(train_images, labels_dir, os.path.join(labels_dir, 'train'))
-    move_image(val_images, images_dir, os.path.join(images_dir, 'val'))
-    move_label(val_images, labels_dir, os.path.join(labels_dir, 'val'))
-    move_image(test_images, images_dir, os.path.join(images_dir, 'test'))
-    move_label(test_images, labels_dir, os.path.join(labels_dir, 'test'))
+    move_files(train_images, images_dir, images_dir / 'train')
+    move_files(train_images, labels_dir, labels_dir / 'train', extension='.txt')
+    move_files(val_images, images_dir, images_dir / 'val')
+    move_files(val_images, labels_dir, labels_dir / 'val', extension='.txt')
+    move_files(test_images, images_dir, images_dir / 'test')
+    move_files(test_images, labels_dir, labels_dir / 'test', extension='.txt')
+    if (labels_dir / 'rotation').exists():
+        print('move rotation labels')
+        if write:
+            for folder in ['train', 'val', 'test']:
+                create_directory(labels_dir / folder / 'rotation')
+        move_files(train_images, labels_dir / 'rotation', labels_dir / 'train' / 'rotation', extension='.txt')
+        move_files(val_images, labels_dir / 'rotation', labels_dir / 'val' / 'rotation', extension='.txt')
+        move_files(test_images, labels_dir / 'rotation', labels_dir / 'test' / 'rotation', extension='.txt')
 
     print("Images split and organized successfully!")
 
 if __name__ == "__main__":
-    dataset_dir = 'data/datasets/bbox-chess3'
-    split_dataset(dataset_dir)
+    dataset_dir = 'data/datasets/bbox5_prod'
+    split_dataset(dataset_dir, write=True, train_ratio=0.98, val_ratio=0.01)
